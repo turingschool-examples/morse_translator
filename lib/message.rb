a82cab6e1b456c21d1e61996daae5d3c0df971ac
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-08-22 13:52:05 -0600

    Setup message

--- lib/message.rb
+++ lib/message.rb
@@ -1,3 +1,11 @@
-class Message
+require './lib/morse_translator'
 
+class Message
+  attr_reader :text
+              :morse_translator
+
+  def initialize(text, morse_translator)
+    @text             = text
+    @morse_translator = morse_translator
+  end
 end
