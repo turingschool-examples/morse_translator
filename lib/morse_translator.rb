662097de97556a92201edf774575971c3ce6fb17
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2018-08-22 14:40:34 -0600

    mental block, cannot move any further man

--- lib/morse_translator.rb
+++ lib/morse_translator.rb
@@ -1,9 +1,9 @@
-# require 'pry'
+
 class MorseTranslator
   attr_reader   :dictionary,
-                :translate
+                # :translate
 
-  def initialize
+  def initialize(translate)
     @dictionary = { "a" => ".-",
                     "b" => "-...",
                     "c" => "-.-.",
@@ -41,14 +41,18 @@ class MorseTranslator
                     "9" => "----.",
                     "0" => "-----",
                     " " => " "}
-
+    # @translate = translate
   end
   def translate
-    translate[:a, :b, :c, :d, :e, :f, :g, :h,
+    translate = @dictionary[:a, :b, :c, :d, :e, :f, :g, :h,
               :i, :j, :k, :l, :m, :n, :o, :p,
               :q, :r, :s, :t, :u, :v, :w, :x,
               :y, :z, 1, 2, 3, 4, 5, 6, 7, 8,
               9, 0, ""]
-    # binding.pry
+#     translate = Hash[keys.split("").zip(values)]
+# MORSE.default=" "
+# def translate(values)
+#   values.downcase.split("").map{|c|translate[c]}.join(" ")
+# # end
   end
 end
