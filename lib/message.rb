1848b1f287a0be1729e18486931c3c9320e3d45e
Cesar Jolibois <diazcd85@gmail.com>
2018-08-22 13:55:45 -0600

    Message class update

--- lib/message.rb
+++ lib/message.rb
@@ -2,4 +2,12 @@ require './lib/morse_translator.rb'
 require 'pry'
 
 class Message
-  def initialize
+    attr_reader :text,
+                :translator
+
+  def initialize(text, translator)
+    @text = text
+    @translator = translator
+  end
+
+end
--- test/test_message.rb
+++ test/test_message.rb
@@ -4,7 +4,18 @@ require './lib/message.rb'
 
 class MessageTest < Minitest::Test
   def test_it_if_exits
+    morse_translator = MorseTranslator.new
     message = Message.new("There are 3 ships", morse_translator)
+
     assert_instance_of Message, message
   end
-end 
+
+  def test_if_it_has_attributes
+    morse = MorseTranslator.new
+    code = Message.new("There are 3 ships", morse)
+
+    assert_equal "There are 3 ships", code.text
+    assert_equal morse, code.translator
+
+  end
+end
