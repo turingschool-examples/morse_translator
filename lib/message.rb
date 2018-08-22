d180ba7ab79d35a7b89ae2e06a1f6f2b675dab6e
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-08-22 14:07:08 -0600

    Write translation method

--- lib/message.rb
+++ lib/message.rb
@@ -1,11 +1,20 @@
 require './lib/morse_translator'
 
 class Message
-  attr_reader :text
+  attr_reader :text,
               :morse_translator
 
   def initialize(text, morse_translator)
     @text             = text
     @morse_translator = morse_translator
+    @reverse_dictionary = @morse_translator.dictionary.invert
+  end
+
+  def translation
+    words = @text.split(" ")
+    translated_text = ""
+    @words.each do |word|
+      translated_text << @reverse_dictionary[word]
+    end
   end
 end
