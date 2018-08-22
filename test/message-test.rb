08d953094bf39930bce5448e8d79103963b08dd1
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-08-22 13:47:12 -0600

    Add message tests

--- test/message-test.rb
+++ test/message-test.rb
@@ -1,7 +1,19 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/morse_translator'
+require './lib/message'
 
 class MessageTest < Minitest::Test
+  def test_it_exists
+      translator = Translator.new
+      message = Message.new("There are 3 ships", translator)
+      assert_instance_of Message , message
+  end
 
+  def test_it_can_translate
+    translator = Translator.new
+    message = Message.new("There are 3 ships", translator)
+    assert_equal "-......-.. .-.-.. ...-- ..........--...." , message.translation
+
+  end
 end
