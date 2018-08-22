832db7754336bc8a0c7bd371b137e98d148873b6
Michael Gatewood <michael@mngatewood.com>
2018-08-22 13:47:10 -0600

    Add getter methods for text and translator and translation method

--- test/message_test.rb
+++ test/message_test.rb
@@ -5,8 +5,31 @@ require './lib/message'
 class MessageTest < Minitest::Test
 
   def test_it_exists
-    message = Message.new
+    morse_translator = MorseTranslator.new
+    message = Message.new("There are 3 ships", morse_translator)
     assert_instance_of Message, message
+  end
+
+  def test_it_has_text
+    morse_translator = MorseTranslator.new
+    message = Message.new("There are 3 ships", morse_translator)
+    actual = message.text
+    expected = "There are 3 ships"
+    assert_equal expected, actual
+  end
+    
+  def test_it_has_a_morse_translator_object
+    morse_translator = MorseTranslator.new
+    message = Message.new("There are 3 ships", morse_translator)
+    assert_instance_of MorseTranslator, message.translator
+  end
+    
+  def test_it_has_translation_method
+    morse_translator = MorseTranslator.new
+    message = Message.new("There are 3 ships", morse_translator)
+    actual = message.translation
+    expected = "-......-.. .-.-.. ...-- ..........--...."
+    assert_equal expected, actual
   end
     
 end
