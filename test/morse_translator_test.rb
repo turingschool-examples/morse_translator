e91b5cb9ef34080de6732c3ae2621e24d0d608de
DanHutch <danhutch5280@gmail.com>
2018-08-22 13:29:48 -0600

    finished iteration 2

--- test/morse_translator_test.rb
+++ test/morse_translator_test.rb
@@ -25,9 +25,11 @@ class MorseTranslatorTest < Minitest::Test
     assert_equal(expected, actual)
   end
 
-
-
-
-
+  def test_it_can_translate_numbers
+    morse_translator = MorseTranslator.new
+    expected = "-......-.. .-.-.. ...-- ..........--...."
+    actual = morse_translator.translate("There are 3 ships")
+    assert_equal(expected, actual)
+  end
 
 end
