31a4fc1a87129ea12dd4d6a2cc306cea97fc9ac5
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-08-22 13:17:21 -0600

    Adds some tests

--- test/morse_translator_test.rb
+++ test/morse_translator_test.rb
@@ -4,5 +4,15 @@ require './lib/morse_translator'
 
 class MorseTranslatorTest < Minitest::Test
 
+  def test_it_exists
+    translator = MorseTranslator.new
+    assert_instance_of MorseTranslator , translator
+  end
+
+  def test_can_translate
+    skip
+    translator = MorseTranslator.new
+    assert_equal "......-...-..--- .-----.-..-..-.." , translator.translate("hello world")
+  end
 
 end
