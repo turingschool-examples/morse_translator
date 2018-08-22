2c6b61047d8c7f814614e4c1527ddda9a7638eaa
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-08-22 13:51:29 -0600

    Add more tests

--- test/message-test.rb
+++ test/message-test.rb
@@ -10,10 +10,22 @@ class MessageTest < Minitest::Test
       assert_instance_of Message , message
   end
 
+  def test_it_has_a_message
+    translator = Translator.new
+    message = Message.new("There are 3 ships", translator)
+    assert_equal "There are 3 ships" , message.text
+  end
+
+  def test_it_has_a_translator
+    translator = Translator.new
+    message = Message.new("There are 3 ships", translator)
+    assert_equal translator , message.translator
+  end
+
   def test_it_can_translate
+    skip
     translator = Translator.new
     message = Message.new("There are 3 ships", translator)
     assert_equal "-......-.. .-.-.. ...-- ..........--...." , message.translation
-
   end
 end
