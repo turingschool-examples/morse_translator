03a5f6472405446a2e6d89ae1413ac10024f475b
Jimmy Smith <jaystansmith@gmail.com>
2018-08-22 14:06:27 -0600

    Added updated code

--- lib/morse_translator.rb
+++ lib/morse_translator.rb
@@ -39,10 +39,62 @@ class MorseTranslator
                     "9" => "----.",
                     "0" => "-----",
                     " " => " "}
-                  end
+
+
+  end
+
   def translate(string)
     string_array = []
     holder = string.chars
-    binding.pry
+    holder.each do |char|
+      dictionary.each do |key,val|
+        if char == key
+          string_array << val
+        end
+      end
+    end
+    string_array.join("")
   end
 end
+
+
+morse_translator = MorseTranslator.new({"a" => ".-",
+                "b" => "-...",
+                "c" => "-.-.",
+                "d" => "-..",
+                "e" => ".",
+                "f" => "..-.",
+                "g" => "--.",
+                "h" => "....",
+                "i" => "..",
+                "j" => ".---",
+                "k" => "-.-",
+                "l" => ".-..",
+                "m" => "--",
+                "n" => "-.",
+                "o" => "---",
+                "p" => ".--.",
+                "q" => "--.-",
+                "r" => ".-.",
+                "s" => "...",
+                "t" => "-",
+                "u" => "..-",
+                "v" => "...-",
+                "w" => ".--",
+                "x" => "-..-",
+                "y" => "-.--",
+                "z" => "--..",
+                "1" => ".----",
+                "2" => "..---",
+                "3" => "...--",
+                "4" => "....-",
+                "5" => ".....",
+                "6" => "-....",
+                "7" => "--...",
+                "8" => "---..",
+                "9" => "----.",
+                "0" => "-----",
+                " " => " "})
+
+"....""."".-.."".-..""---"" "".--""---"".-."".-..""-.."
+p morse_translator.translate("hello world")
