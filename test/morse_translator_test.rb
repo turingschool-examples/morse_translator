6293a1fc83823ec8d4500dfe3166e61825b38b27
Michael Gatewood <michael@mngatewood.com>
2018-08-22 13:10:16 -0600

    Adds test it exists to morse_translator_test

---
+++ test/morse_translator_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/morse_translator'
+
+class MorseTranslatorTest < Minitest::Test
+
+  def test_it_exists
+    morse_translator = MorseTranslator.new()
+    assert_instance_of MorseTranslator, morse_translator
+  end
+  
+end
